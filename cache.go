9ba7c57df5af9757fa9c62212b7b2eba10daca2c
Sean Wong <wotzhs@gmail.com>
2021-10-10 00:39:08 +0800

    cache: add basic caching mechanisms

---
+++ cache.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	CSVHeader []string = []string{"id", "name", "types", "stats", "encounters", "cached_at"}
+)
+
+type CacheEntry struct {
+	Pokemon  Pokemon
+	CachedAt time.Time
+}
+
+type Cache struct {
+	NameMap map[string]string
+	IDMap   map[string]CacheEntry
+}
+
+const (
+	pos_ID int = iota
+	pos_NAME
+	pos_TYPES
+	pos_STATS
+	pos_ENCOUNTERS
+	pos_CACHED_AT
+)
+
+func (c *Cache) Restore() error {
+	// prevent wrongful restore
+	if c.NameMap != nil && c.IDMap != nil {
+		return nil
+	}
+
+	c.NameMap = map[string]string{}
+	c.IDMap = map[string]CacheEntry{}
+
+	f, err := os.OpenFile("cache", os.O_RDONLY, 0444)
+	if err != nil {
+		return err
+	}
+
+	r := csv.NewReader(f)
+
+	// skip header
+	r.Read()
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			continue
+		}
+
+		ID, err := strconv.Atoi(record[pos_ID])
+		if err != nil {
+			continue
+		}
+
+		types := []Type{}
+		if err := json.Unmarshal([]byte(record[pos_TYPES]), &types); err != nil {
+			continue
+		}
+
+		stats := []Stat{}
+		if err := json.Unmarshal([]byte(record[pos_STATS]), &stats); err != nil {
+			continue
+		}
+
+		pokemon := Pokemon{
+			ID:         ID,
+			Name:       record[pos_NAME],
+			Types:      types,
+			Stats:      stats,
+			Encounters: "-",
+		}
+
+		if record[pos_ENCOUNTERS] != "-" {
+			encounters := []PokeomonEncounterLocations{}
+			if err := json.Unmarshal([]byte(record[pos_ENCOUNTERS]), &encounters); err != nil {
+				continue
+			}
+			pokemon.Encounters = encounters
+		}
+
+		cachedAt, err := time.Parse(time.RFC3339, record[pos_CACHED_AT])
+		if err != nil {
+			continue
+		}
+
+		c.NameMap[pokemon.Name] = record[pos_ID]
+		c.IDMap[record[pos_ID]] = CacheEntry{
+			Pokemon:  pokemon,
+			CachedAt: cachedAt,
+		}
+	}
+
+	return nil
+}
+
+func (c Cache) WriteToCSV() error {
+	f, err := os.OpenFile("cache", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+
+	w := csv.NewWriter(f)
+
+	records := [][]string{CSVHeader}
+	for name, id := range c.NameMap {
+		cacheEntry, ok := c.IDMap[id]
+		if !ok {
+			continue
+		}
+
+		var (
+			typesBuffer      bytes.Buffer
+			statsBuffer      bytes.Buffer
+			encountersBuffer bytes.Buffer
+		)
+
+		if err := json.NewEncoder(&typesBuffer).Encode(cacheEntry.Pokemon.Types); err != nil {
+			continue
+		}
+
+		if err := json.NewEncoder(&statsBuffer).Encode(cacheEntry.Pokemon.Stats); err != nil {
+			continue
+		}
+
+		if err := json.NewEncoder(&encountersBuffer).Encode(cacheEntry.Pokemon.Encounters); err != nil {
+			continue
+		}
+
+		records = append(records, []string{
+			id,
+			name,
+			typesBuffer.String(),
+			statsBuffer.String(),
+			encountersBuffer.String(),
+			cacheEntry.CachedAt.Format(time.RFC3339),
+		})
+	}
+
+	w.WriteAll(records)
+
+	return w.Error()
+}
+
+func (c Cache) FindPokemon(input string) (*Pokemon, bool) {
+	isID := true
+	for _, char := range input {
+		if char < '0' || char > '9' {
+			isID = false
+			break
+		}
+	}
+
+	identifier := input
+	if !isID {
+		if ID, ok := c.NameMap[identifier]; ok {
+			identifier = ID
+		}
+	}
+
+	if entry, ok := c.IDMap[identifier]; ok {
+		return &entry.Pokemon, true
+	}
+	return nil, false
+}
+
+func (c Cache) CachePokemon(pokemon Pokemon) error {
+	pokemonID := strconv.Itoa(pokemon.ID)
+	c.NameMap[pokemon.Name] = pokemonID
+	c.IDMap[pokemonID] = CacheEntry{
+		Pokemon:  pokemon,
+		CachedAt: time.Now(),
+	}
+
+	return c.WriteToCSV()
+}
--- main.go
+++ main.go
@@ -2,20 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
 func main() {
+	cache := Cache{}
+	if err := cache.Restore(); err != nil {
+		if !os.IsNotExist(err) {
+			fmt.Printf("failed to restore cache err: %v\n", err)
+			fmt.Println("Re-creating a new cache")
+		}
+	}
+
 	var cmdFind = &cobra.Command{
 		Use:   "find [id or name]",
 		Short: "find a pokemen by id or name",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			pokemon := &Pokemon{PokeAPIBaseURL: "https://pokeapi.co/api/v2"}
-			if err := pokemon.Find(args[0]); err != nil {
-				fmt.Printf("failed to find pokemon, err: %v\n", err)
-				return
+			pokemon, found := cache.FindPokemon(args[0])
+
+			if !found {
+				pokemon = &Pokemon{PokeAPIBaseURL: "https://pokeapi.co/api/v2"}
+				fmt.Println("fetching from " + pokemon.PokeAPIBaseURL)
+
+				if err := pokemon.Find(args[0]); err != nil {
+					fmt.Printf("failed to find pokemon, err: %v\n", err)
+					return
+				}
+				if err := cache.CachePokemon(*pokemon); err != nil {
+					fmt.Println("failed to update cache")
+				}
+			}
+
+			if found {
+				fmt.Println("served from cache")
 			}
 
 			pokemonDetails, err := pokemon.PrettyPrint()
